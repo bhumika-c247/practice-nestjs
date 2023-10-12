f78585fa79bc327a26b4dc03ece0ca8cbbba2250
bhumika-c247 <bhoomika.b@chapter247.com>
2023-10-12 14:11:09 +0530

    feat: created user crud

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,40 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
   @UseGuards(AuthGuard('jwt'))
   @Get('/me')
   getUserDetails(@Req() req: Request) {
     return req.user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.delete(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, Param } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 export type User = any;
 
@@ -16,6 +17,54 @@ export class UsersService {
         },
       });
       return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async findAll() {
+    try {
+      const users = await this.prisma.user.findMany();
+      return users;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async update(@Param('id') id: string, dto: UpdateUserDto) {
+    try {
+      const checkExistingUser = await this.prisma.user.findUnique({
+        where: {
+          id,
+        },
+      });
+      if (!checkExistingUser) throw new ForbiddenException('User not found');
+      const updateUser = await this.prisma.user.update({
+        where: {
+          id,
+        },
+        data: { ...dto },
+      });
+      return updateUser;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async delete(@Param('id') id: string) {
+    try {
+      const checkExistingUser = await this.prisma.user.findUnique({
+        where: {
+          id,
+        },
+      });
+      if (!checkExistingUser) throw new ForbiddenException('User not found');
+      const deleteUser = await this.prisma.user.delete({
+        where: {
+          id,
+        },
+      });
+      return { user: deleteUser.id, message: 'User deleted' };
     } catch (error) {
       throw error;
     }
